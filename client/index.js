4364f28aea75ef1d498614f8b2b7fc4df5631eef
Akhouri Vishal Sinha <vakhouri@gmail.com>
2017-04-11 01:22:43 +1000

    added reaact and react-dom

--- client/index.js
+++ client/index.js
@@ -1,1 +1,15 @@
-console.log('Hello ladies and gentlemen!');
+/**import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+//const App = function() {
+//  return <div>Hello World</div>;
+//}
+
+ReactDOM.render(<App />, document.getElementById('root'));
